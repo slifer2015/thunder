00d85699ff824b840e926d67383fdccb295a9502
slifer2015 <dara51php@gmail.com>
2016-06-19 23:48:44 +0430

    first commit

--- config.php
+++ config.php
@@ -12,9 +12,9 @@ ob_start();
 session_start();
 error_reporting(0);
 return $_Config = array(
-    'DB_SERVER' => 'localhost',// db server
+    'DB_SERVER' => '78.46.11.233',// db server
     'DB_USER' => 'root',// db user
-    'DB_PASSWORD' => 'root',// db password (mention your db password here)
+    'DB_PASSWORD' => '5109681A3',// db password (mention your db password here)
     'DB_NAME' => 'socialnetwork',// database name
     'DB_TABLE_PREFIX' => 'fa_'//database prefix
 );
